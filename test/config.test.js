92b270d398b19504d84d79850a59c52056f67e5c
Sergey Tatarintsev <sevinf@yandex-team.ru>
2014-08-05 18:57:24 +0400

    Refactor config class
    Move options parsing off the constructor to separate module
    to make code more readable and easier to add new options.

--- test/config.test.js
+++ test/config.test.js
@@ -3,33 +3,164 @@ var Config = require('../lib/config'),
     GeminiError = require('../lib/errors/gemini-error');
 
 describe('config', function() {
-    it('should require root url', function() {
-        (function() {
-            return new Config('/', [
+    describe('rootUrl', function() {
+        it('should be required', function() {
+            (function() {
+                return new Config('/', [
+                    'gridUrl: http://example.com'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should accept strings', function() {
+            var config = new Config('/', 'rootUrl: http://example.com');
+            config.rootUrl.must.be('http://example.com');
+        });
+
+        it('should be overridable', function() {
+            var config = new Config('/', 'rootUrl: http://example.com', {
+                rootUrl: 'http://example.org'
+            });
+
+            config.rootUrl.must.be('http://example.org');
+        });
+
+        it('should be settable via overrides', function() {
+            var config = new Config('/', 'gridUrl: http://example.com', {
+                rootUrl: 'http://example.org'
+            });
+
+            config.rootUrl.must.be('http://example.org');
+        });
+    });
+
+    describe('gridUrl', function() {
+        it('should be required is there are non-phantomjs browsers', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'browsers:',
+                    '  phantomjs: phantomjs',
+                    '  non-phantomjs: non-phantomjs'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should not be required if there are only phantomjs browser', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'browsers:',
+                    '  phantomjs: phantomjs'
+                ].join('\n'));
+            }.must.not.throw());
+        });
+
+        it('should accept string', function() {
+            var config = new Config('/', [
+                'gridUrl: http://grid.example.com',
+                'rootUrl: http://example.com'
+            ].join('\n'));
+            config.gridUrl.must.be('http://grid.example.com');
+        });
+
+        it('should be overridable', function() {
+            var config = new Config('/', [
+                'gridUrl: http://grid.example.com',
+                'rootUrl: http://example.com'
+            ].join('\n'), {
+                gridUrl: 'http://grid.example.org'
+            });
+            config.gridUrl.must.be('http://grid.example.org');
+        });
+
+        it('should be settable with overrides', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com'
+            ].join('\n'), {
+                gridUrl: 'http://grid.example.org'
+            });
+            config.gridUrl.must.be('http://grid.example.org');
+        });
+    });
+
+    describe('browsers', function() {
+        it('should accept objects', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'browsers:',
+                '  someBrowser:',
+                '    browserName: bro',
+                '    capability: cap'
+            ].join('\n'));
+            config.browsers.must.eql({
+                someBrowser: {
+                    browserName: 'bro',
+                    capability: 'cap'
+                }
+            });
+        });
+
+        it('should transform "id: name" to valid capabilites', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'browsers:',
+                '  someBrowser: bro'
+            ].join('\n'));
+            config.browsers.must.eql({
+                someBrowser: {
+                    browserName: 'bro'
+                }
+            });
+        });
+
+        it('should accept legacy array', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'browsers:',
+                '  - {name: bro}',
+                '  - {name: bro2, version: "12.0"}',
+                '  - bro3'
+            ].join('\n'));
+            config.browsers.must.eql({
+                bro: {
+                    browserName: 'bro'
+                },
+                'bro2-v12.0': {
+                    browserName: 'bro2',
+                    version: '12.0'
+                },
+
+                bro3: {
+                    browserName: 'bro3'
+                }
+            });
+        });
+
+        it('should be phantomjs by default', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
                 'gridUrl: http://example.com'
             ].join('\n'));
-        }.must.throw(GeminiError));
-    });
+            config.browsers.must.eql({
+                phantomjs: {
+                    browserName: 'phantomjs'
+                }
+            });
+        });
 
-    it('should require grid url is there are non-phantomjs browsers', function() {
-        (function() {
-            return new Config('/', [
-                'rootUrl: http://example.com',
-                'browsers:',
-                '  phantomjs: phantomjs',
-                '  non-phantomjs: non-phantomjs'
-            ].join('\n'));
-        }.must.throw(GeminiError));
-    });
-
-    it('should not require grid url if there are only phantomjs browser', function() {
-        (function() {
-            return new Config('/', [
-                'rootUrl: http://example.com',
-                'browsers:',
-                '  phantomjs: phantomjs'
-            ].join('\n'));
-        }.must.not.throw());
+        it('should throw if not object nor array', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'browsers: yep!'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
     });
 
     describe('capabilities', function() {
@@ -81,6 +212,39 @@ describe('config', function() {
                 retries: 5,
                 retryDelay: 25
             });
+        });
+
+        it('should not accept non-number timeout', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'http:',
+                    '  timeout: not a number'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should not accept non-number retires', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'http:',
+                    '  retries: not a number'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should not accept non-number retryDelay', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'http:',
+                    '  retryDelay: not a number'
+                ].join('\n'));
+            }.must.throw(GeminiError));
         });
     });
 
@@ -212,6 +376,65 @@ describe('config', function() {
                 'diffColor: "#ff0000"'
             ].join('\n'));
             config.diffColor.must.be('#ff0000');
+        });
+    });
+
+    describe('debug', function() {
+        it('should not accept non-boolean', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'debug: very much'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should accept true', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'debug: true'
+            ].join('\n'));
+            config.debug.must.be(true);
+        });
+
+        it('should accept false', function() {
+            var config = new Config('/', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'debug: false'
+            ].join('\n'));
+            config.debug.must.be(false);
+        });
+    });
+
+    describe('screenshotsDir', function() {
+        it('should not accept non-string value', function() {
+            (function() {
+                return new Config('/', [
+                    'rootUrl: http://example.com',
+                    'gridUrl: http://example.com',
+                    'screenshotsDir: 12.5'
+                ].join('\n'));
+            }.must.throw(GeminiError));
+        });
+
+        it('should be a file path resolved relative to root', function() {
+            var config = new Config('/some/path/config.yml', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com',
+                'screenshotsDir: screens'
+            ].join('\n'));
+            config.screenshotsDir.must.be('/some/path/screens');
+        });
+
+        it('should be gemini/screens by default', function() {
+            var config = new Config('/some/path/config.yml', [
+                'rootUrl: http://example.com',
+                'gridUrl: http://example.com'
+            ].join('\n'));
+            config.screenshotsDir.must.be('/some/path/gemini/screens');
         });
     });
 });
