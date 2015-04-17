2ed90f6ba57c65292a73b9dd3a1e89e59777564b
Sergey Tatarintsev <sevinf@yandex-team.ru>
2015-04-17 11:41:31 +0300

    Migrate suite-util tests to chai.assert

--- test/suite-util.test.js
+++ test/suite-util.test.js
@@ -18,39 +18,39 @@ describe('suite-util', function() {
         }
 
         it('should not skip any browser if skipped=false', function() {
-            shouldSkip(false, makeBrowser({browserName: 'browser'})).must.be.false();
+            assert.isFalse(shouldSkip(false, makeBrowser({browserName: 'browser'})));
         });
 
         it('should skip any browser if skipped=true', function() {
-            shouldSkip(true, makeBrowser({browserName: 'browser'})).must.be.true();
+            assert.isTrue(shouldSkip(true, makeBrowser({browserName: 'browser'})));
         });
 
         it('should skip browser if its name and version matches skip list', function() {
-            shouldSkip(
+            assert.isTrue(shouldSkip(
                 [{browserName: 'browser', version: '1.0'}],
                 makeBrowser({browserName: 'browser', version: '1.0'})
-            ).must.be.true();
+            ));
         });
 
         it('should not skip the browser if its name does not match skip list', function() {
-            shouldSkip(
+            assert.isFalse(shouldSkip(
                 [{browserName: 'browser', version: '1.0'}],
                 makeBrowser({browserName: 'other browser', version: '1.0'})
-            ).must.be.false();
+            ));
         });
 
         it('should not skip browser if its version does not match skip list', function() {
-            shouldSkip(
+            assert.isFalse(shouldSkip(
                 [{browserName: 'browser', version: '1.0'}],
                 makeBrowser({browserName: 'browser', version: '1.1'})
-            ).must.be.false();
+            ));
         });
 
         it('should skip any browser of a given name if version is not specified in skip list', function() {
-            shouldSkip(
+            assert.isTrue(shouldSkip(
                 [{browserName: 'browser'}],
                 makeBrowser({browserName: 'browser', version: '1.1'})
-            ).must.be.true();
+            ));
         });
     });
 
