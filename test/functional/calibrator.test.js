aaeb9c2c338a5afc97eda5ccc431e38802354d65
Vladimir Alaev <scff@yandex-team.ru>
2015-05-27 15:54:01 +0300

    Disable mocha timeouts

--- test/functional/calibrator.test.js
+++ test/functional/calibrator.test.js
@@ -6,17 +6,14 @@ var eachSupportedBrowser = require('./util').eachSupportedBrowser,
 describe('calibrator', function() {
     eachSupportedBrowser(function() {
         beforeEach(function() {
-            this.timeout(30000);
             return this.browser.initSession();
         });
 
         afterEach(function() {
-            this.timeout(10000);
             return this.browser.quit();
         });
 
         it('should not fail', function() {
-            this.timeout(10000);
             var calibrator = new Calibrator();
             return assert.isFulfilled(calibrator.calibrate(this.browser));
         });
--- test/functional/client-bridge.test.js
+++ test/functional/client-bridge.test.js
@@ -9,18 +9,15 @@ var ClientBridge = require('../../lib/browser/client-bridge'),
 describe('ClientBridge', function() {
     eachSupportedBrowser(function() {
         beforeEach(function() {
-            this.timeout(30000);
             this.bridge = new ClientBridge(this.browser, TEST_SCRIPT);
             return this.browser.initSession();
         });
 
         afterEach(function() {
-            this.timeout(20000);
             return this.browser.quit();
         });
 
         it('should succesfully perform client method call', function() {
-            this.timeout(20000);
             return assert.becomes(this.bridge.call('add2', [1]), 3);
         });
     });
