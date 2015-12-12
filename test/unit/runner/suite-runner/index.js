25ecdd2f3d8c89733f5b65dd33e14f7934aed9cc
Anton Usmansky <cody0@yandex-team.ru>
2015-12-13 00:12:26 +0300

    Do not capture session for skipped suite

--- test/unit/runner/suite-runner/index.js
+++ test/unit/runner/suite-runner/index.js
@@ -3,17 +3,15 @@
 var suiteRunner = require('../../../../lib/runner/suite-runner'),
     RegularSuiteRunner = require('../../../../lib/runner/suite-runner/regular-suite-runner'),
     StatelessSuiteRunner = require('../../../../lib/runner/suite-runner/stateless-suite-runner'),
+    SkippedSuiteRunner = require('../../../../lib/runner/suite-runner/skipped-suite-runner'),
+    suiteUtil = require('../../../../lib/suite-util'),
     util = require('../../../util');
 
 describe('runner/suite-runner/create', function() {
-    it('should create RegularSuiteRunner for suite with states', function() {
-        var suite = util.makeSuiteStub({
-                states: [util.makeStateStub()]
-            });
+    var sandbox = sinon.sandbox.create();
 
-        var runner = suiteRunner.create(suite);
-
-        assert.instanceOf(runner, RegularSuiteRunner);
+    afterEach(function() {
+        sandbox.restore();
     });
 
     it('should create StatelessSuiteRunner for suite without states', function() {
@@ -22,5 +20,26 @@ describe('runner/suite-runner/create', function() {
         var runner = suiteRunner.create(suite);
 
         assert.instanceOf(runner, StatelessSuiteRunner);
+    });
+
+    it('should create RegularSuiteRunner for suite with states', function() {
+        var suite = util.makeSuiteStub({
+                states: [util.makeStateStub()]
+            });
+
+        var runner = suiteRunner.create(suite, {});
+
+        assert.instanceOf(runner, RegularSuiteRunner);
+    });
+
+    it('should create SkippedSuiteRunner for skipped suite', function() {
+        var suite = util.makeSuiteStub({
+                states: [util.makeStateStub()]
+            });
+        sandbox.stub(suiteUtil, 'shouldSkip').returns(true);
+
+        var runner = suiteRunner.create(suite, {});
+
+        assert.instanceOf(runner, SkippedSuiteRunner);
     });
 });
