06f2bafd7f0570d02b622fb0fb77d16d64494ec8
Anton Usmansky <cody0@yandex-team.ru>
2016-04-20 15:26:43 +0300

    Refactoring: moved config file reader into separate module

---
+++ test/unit/config/config-reader.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs'),
+    path = require('path'),
+    proxyquire = require('proxyquire').noCallThru(),
+    _ = require('lodash');
+
+describe('config/config-reader', () => {
+    const sandbox = sinon.sandbox.create();
+
+    beforeEach(() => {
+        sandbox.stub(fs, 'readdirSync');
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    function initReader_(files) {
+        fs.readdirSync.returns(_.keys(files));
+
+        return proxyquire('../../../lib/config/config-reader', files);
+    }
+
+    describe('default config (js|json)', () => {
+        function test_(fileName) {
+            it('should read ' + fileName, () => {
+                const files = {};
+                files[fileName] = {some: 'data'};
+                const reader = initReader_(files);
+
+                const result = reader.read();
+
+                assert.deepEqual(result, {some: 'data'});
+            });
+        }
+
+        test_('.gemini.conf.js');
+        test_('.gemini.js');
+        test_('.gemini.conf.json');
+        test_('.gemini.json');
+    });
+});
