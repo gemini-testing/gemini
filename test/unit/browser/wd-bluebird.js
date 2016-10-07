c98b9fa1e6c0ea42bc6b4a905fe8707306e0af67
Sergey Tatarintsev <sergey.tatarintsev@researchgate.net>
2016-10-07 14:10:45 +0200

    Cast result of all wd method to Bluebird promises
    Introduce small wd-bluebird module, which just wraps existing
    wd methods and cast the result to Blubird promise.

---
+++ test/unit/browser/wd-bluebird.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Promise = require('bluebird');
+const q = require('bluebird-q');
+const proxyquire = require('proxyquire');
+
+describe('wd-bluebird', () => {
+    const sandbox = sinon.sandbox.create();
+    let original, wrapped;
+
+    beforeEach(() => {
+        original = {
+            promiseMethod: () => q('result'),
+            emit() {},
+            property: 'value'
+        };
+
+        const wd = proxyquire('lib/browser/wd-bluebird', {
+            wd: {
+                promiseRemote: () => original
+            }
+        });
+
+        wrapped = wd.promiseRemote();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should cast original promise to Bluebird', () => {
+        assert.instanceOf(
+            wrapped.promiseMethod(),
+            Promise
+        );
+    });
+
+    it('should resolve to original value', () => {
+        return assert.eventually.equal(
+            wrapped.promiseMethod(),
+            original.promiseMethod()
+        );
+    });
+
+    it('should forward all arguments to original method', () => {
+        const arg = 'arg';
+        sandbox.spy(original, 'promiseMethod');
+
+        wrapped.promiseMethod(arg);
+
+        assert.calledWith(original.promiseMethod, arg);
+    });
+
+    it('should copy properties as is', () => {
+        assert.equal(wrapped.property, original.property);
+    });
+
+    it('should not cast event emitter methods results', () => {
+        assert.isUndefined(
+            wrapped.emit('event')
+        );
+    });
+
+    it('should not cast Object.prototype methods results', () => {
+        assert.isString(
+            wrapped.toString()
+        );
+    });
+});
