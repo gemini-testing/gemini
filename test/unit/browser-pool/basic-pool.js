e8a427e13600997807fd300f034aa76ac26a516e
Anton Usmansky <cody0@yandex-team.ru>
2016-11-22 17:46:47 +0300

    fix: emit browser start/stop events in correct places

--- test/unit/browser-pool/basic-pool.js
+++ test/unit/browser-pool/basic-pool.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const Promise = require('bluebird');
+const QEmitter = require('qemitter');
+const bluebirdQ = require('bluebird-q');
 const Browser = require('lib/browser');
 const BasicPool = require('lib/browser-pool/basic-pool');
 const CancelledError = require('lib/errors/cancelled-error');
 const browserWithId = require('test/util').browserWithId;
+const Events = require('lib/constants/events');
+const _ = require('lodash');
 
 describe('BasicPool', () => {
     const sandbox = sinon.sandbox.create();
 
-    const stubBrowser = (id) => {
+    const stubBrowser_ = (id) => {
         id = id || 'id';
         const browser = browserWithId(id);
         sandbox.stub(browser, 'launch', () => {
@@ -26,66 +30,164 @@ describe('BasicPool', () => {
 
     const stubConfig = () => ({forBrowser: (id) => ({id})});
 
-    let pool;
+    const mkPool_ = (opts) => {
+        opts = _.defaults(opts || {}, {
+            config: stubConfig(),
+            calibrator: 'default-calibrator',
+            emitter: new QEmitter()
+        });
 
-    beforeEach(() => {
-        pool = new BasicPool(stubConfig());
+        return new BasicPool(opts.config, opts.calibrator, opts.emitter);
+    };
 
-        sandbox.stub(Browser, 'create');
-    });
-    afterEach(() => sandbox.restore());
-
-    const requestBrowser = (browser) => {
-        browser = browser || stubBrowser();
+    const requestBrowser_ = (browser, pool) => {
+        browser = browser || stubBrowser_();
+        pool = pool || mkPool_();
 
         return pool.getBrowser(browser.id);
     };
 
-    it('should create new browser when requested', () => {
-        const browser = stubBrowser('foo');
+    beforeEach(() => {
+        sandbox.stub(Browser, 'create');
+    });
 
-        return requestBrowser(browser)
+    afterEach(() => sandbox.restore());
+
+    it('should create new browser when requested', () => {
+        const browser = stubBrowser_('foo');
+
+        return requestBrowser_(browser)
             .then(() => assert.calledWith(Browser.create, {id: 'foo'}));
     });
 
     it('should launch a browser', () => {
-        const browser = stubBrowser();
+        const browser = stubBrowser_();
 
-        return requestBrowser(browser)
+        return requestBrowser_(browser)
             .then(() => assert.calledOnce(browser.launch));
     });
 
     it('should launch a browser with calibrator', () => {
-        pool = new BasicPool(stubConfig(), 'calibrator');
+        const pool = mkPool_({calibrator: 'calibrator'});
 
-        const browser = stubBrowser();
+        const browser = stubBrowser_();
 
-        return requestBrowser(browser)
+        return requestBrowser_(browser, pool)
             .then(() => assert.calledWith(browser.launch, 'calibrator'));
     });
 
     it('should finalize browser if failed to create it', () => {
-        const browser = stubBrowser();
+        const browser = stubBrowser_();
+        const pool = mkPool_();
         const freeBrowser = sinon.spy(pool, 'freeBrowser');
         const assertCalled = () => assert.called(freeBrowser);
 
-        browser.reset.returns(Promise.reject());
+        browser.reset.returns(bluebirdQ.reject());
 
-        return requestBrowser(browser)
+        return requestBrowser_(browser, pool)
             .then(assertCalled, assertCalled);
     });
 
-    it('should quit a browser when freed', () => {
-        const browser = stubBrowser();
+    describe('START_BROWSER event', () => {
+        it('should be emitted on browser start', () => {
+            const emitter = new QEmitter();
+            const onSessionStart = sinon.spy().named('onSessionStart');
+            emitter.on(Events.START_BROWSER, onSessionStart);
 
-        return requestBrowser(browser)
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return requestBrowser_(browser, pool)
+                .then(() => {
+                    assert.calledOnce(onSessionStart);
+                    assert.calledWith(onSessionStart, browser);
+                });
+        });
+
+        it('handler should be waited by pool', () => {
+            const emitter = new QEmitter();
+            const afterSessionStart = sinon.spy().named('afterSessionStart');
+            emitter.on(Events.START_BROWSER, () => bluebirdQ.delay(100).then(afterSessionStart));
+
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return requestBrowser_(browser, pool)
+                .then(() => assert.callOrder(afterSessionStart, browser.reset));
+        });
+
+        it('handler fail should fail browser request', () => {
+            const emitter = new QEmitter();
+            emitter.on(Events.START_BROWSER, () => bluebirdQ.reject('some-error'));
+
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return assert.isRejected(requestBrowser_(browser, pool), 'some-error');
+        });
+    });
+
+    it('should quit a browser when freed', () => {
+        const pool = mkPool_();
+        const browser = stubBrowser_();
+
+        return requestBrowser_(browser, pool)
             .then((browser) => pool.freeBrowser(browser))
             .then(() => assert.calledOnce(browser.quit));
     });
 
+    describe('STOP_BROWSER event', () => {
+        it('should be emitted on browser quit', () => {
+            const emitter = new QEmitter();
+            const onSessionEnd = sinon.spy().named('onSessionEnd');
+            emitter.on(Events.STOP_BROWSER, onSessionEnd);
+
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return requestBrowser_(browser, pool)
+                .then((browser) => pool.freeBrowser(browser))
+                .then(() => {
+                    assert.calledOnce(onSessionEnd);
+                    assert.calledWith(onSessionEnd, browser);
+                });
+        });
+
+        it('handler should be waited before actual quit', () => {
+            const emitter = new QEmitter();
+            const afterSessionEnd = sinon.spy().named('afterSessionEnd');
+            emitter.on(Events.STOP_BROWSER, () => bluebirdQ.delay(100).then(afterSessionEnd));
+
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return requestBrowser_(browser, pool)
+                .then((browser) => pool.freeBrowser(browser))
+                .then(() => assert.callOrder(afterSessionEnd, browser.quit));
+        });
+
+        it('handler fail should not prevent browser from quit', () => {
+            const emitter = new QEmitter();
+            emitter.on(Events.STOP_BROWSER, () => bluebirdQ.reject());
+
+            const pool = mkPool_({emitter});
+            const browser = stubBrowser_();
+
+            return requestBrowser_(browser, pool)
+                .then((browser) => pool.freeBrowser(browser))
+                .then(() => assert.calledOnce(browser.quit));
+        });
+    });
+
     describe('cancel', () => {
         it('should quit all browsers on cancel', () => {
-            return Promise.all([requestBrowser(stubBrowser('id1')), requestBrowser(stubBrowser('id2'))])
+            const pool = mkPool_();
+
+            return Promise
+                .all([
+                    requestBrowser_(stubBrowser_('id1'), pool),
+                    requestBrowser_(stubBrowser_('id2'), pool)
+                ])
                 .spread((firstBrowser, secondBrowser) => {
                     pool.cancel();
 
@@ -95,7 +197,13 @@ describe('BasicPool', () => {
         });
 
         it('should quit all browser with the same id on cancel', () => {
-            return Promise.all([requestBrowser(stubBrowser('id')), requestBrowser(stubBrowser('id'))])
+            const pool = mkPool_();
+
+            return Promise
+                .all([
+                    requestBrowser_(stubBrowser_('id'), pool),
+                    requestBrowser_(stubBrowser_('id'), pool)
+                ])
                 .spread((firstBrowser, secondBrowser) => {
                     pool.cancel();
 
@@ -105,24 +213,28 @@ describe('BasicPool', () => {
         });
 
         it('should be rejected if getting of a browser was called after cancel', () => {
+            const pool = mkPool_();
+
             pool.cancel();
 
-            return assert.isRejected(requestBrowser(stubBrowser()), CancelledError);
+            return assert.isRejected(requestBrowser_(stubBrowser_(), pool), CancelledError);
         });
 
         it('should quit a browser once if it was launched after cancel', () => {
-            const browser = stubBrowser();
+            const pool = mkPool_();
+            const browser = stubBrowser_();
 
             pool.cancel();
 
-            return requestBrowser(browser)
+            return requestBrowser_(browser, pool)
                 .catch(() => assert.calledOnce(browser.quit));
         });
 
         it('should clean active sessions after cancel', () => {
-            const browser = stubBrowser();
+            const pool = mkPool_();
+            const browser = stubBrowser_();
 
-            return requestBrowser(browser)
+            return requestBrowser_(browser, pool)
                 .then(() => pool.cancel())
                 .then(() => pool.cancel())
                 .then(() => assert.calledOnce(browser.quit));
